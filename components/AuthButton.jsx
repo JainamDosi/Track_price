ffc83f1b858cbc9fd15ce25013108fa4a6b7e414
Jainam <127018175+JainamDosi@users.noreply.github.com>
2025-12-23 19:14:14 +0530

    Auth added

---
+++ components/AuthButton.jsx
@@ -0,0 +1,7 @@
+
+
+export default function AuthButton() {
+    return (
+        <button>AuthButton</button>
+    )
+}
---
+++ components/AuthModal.js
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function AuthModal({isOpen,onClose}) {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+   
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          
+        
+        </DialogContent>
+      
+    </Dialog>
+  )
+}
